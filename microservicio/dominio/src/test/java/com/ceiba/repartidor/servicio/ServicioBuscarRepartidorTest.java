b9d515cad30cc14a10c4274f4a3925ad99ffb8d6
Didier Gaona <didier.gaona@ceiba.com.co>
2022-01-17 11:54:50 -0500

    Se agrega test para buscar repartidor en repositorio

--- microservicio/dominio/src/test/java/com/ceiba/repartidor/servicio/ServicioBuscarRepartidorTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/repartidor/servicio/ServicioBuscarRepartidorTest.java
@@ -2,10 +2,8 @@ package com.ceiba.repartidor.servicio;
 
 import com.ceiba.BasePrueba;
 import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
-import com.ceiba.repartidor.modelo.entidad.Repartidor;
 import com.ceiba.repartidor.puerto.dao.DaoRepartidor;
 import com.ceiba.repartidor.puerto.repositorio.RepositorioRepartidor;
-import com.ceiba.repartidor.servicio.testdatabuilder.RepartidorTestDataBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -29,13 +27,14 @@ class ServicioBuscarRepartidorTest {
     @DisplayName("Deberia buscar correctamente en el repositorio")
     void deberiaBuscarCorrectamenteEnElRepositorio() {
         // arrange
-        Repartidor repartidor = new RepartidorTestDataBuilder().conId(1L).build();
+        Long idRepartidor = 1L;
         RepositorioRepartidor repositorioRepartidor = Mockito.mock(RepositorioRepartidor.class);
+        DaoRepartidor daoRepartidor = Mockito.mock(DaoRepartidor.class);
         Mockito.when(repositorioRepartidor.existePorId(Mockito.anyLong())).thenReturn(true);
-        ServicioActualizarRepartidor servicioActualizarRepartidor = new ServicioActualizarRepartidor(repositorioRepartidor);
+        ServicioBuscarRepartidor servicioBuscarRepartidor = new ServicioBuscarRepartidor(daoRepartidor, repositorioRepartidor);
         // act
-        servicioActualizarRepartidor.ejecutar(repartidor);
+        servicioBuscarRepartidor.ejecutar(idRepartidor);
         //assert
-        Mockito.verify(repositorioRepartidor, Mockito.times(1)).actualizar(repartidor);
+        Mockito.verify(daoRepartidor, Mockito.times(1)).buscarPorId(idRepartidor);
     }
 }
