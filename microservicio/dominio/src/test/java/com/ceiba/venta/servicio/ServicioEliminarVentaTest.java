88258dc145f6d09d494117ef91459583c4fa710f
Didier Gaona <didier.gaona@ceiba.com.co>
2022-01-19 14:55:39 -0500

    Correcciones Sonarqube y más tests

--- microservicio/dominio/src/test/java/com/ceiba/venta/servicio/ServicioEliminarVentaTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/venta/servicio/ServicioEliminarVentaTest.java
@@ -1,7 +1,9 @@
 package com.ceiba.venta.servicio;
 
+import com.ceiba.item.modelo.dto.DtoItem;
 import com.ceiba.item.puerto.dao.DaoItem;
 import com.ceiba.item.puerto.repositorio.RepositorioItem;
+import com.ceiba.item.servicio.testdatabuilder.DtoItemTestDataBuilder;
 import com.ceiba.venta.modelo.dto.DtoVenta;
 import com.ceiba.venta.puerto.dao.DaoVenta;
 import com.ceiba.venta.puerto.repositorio.RepositorioVenta;
@@ -21,12 +23,14 @@ class ServicioEliminarVentaTest {
         // arrange
         List<Long> ids = Collections.singletonList(1L);
         DtoVenta dtoVenta = new DtoVentaTestDataBuilder().build();
+        List<DtoItem> dtoItemsExistentes = Collections.singletonList(new DtoItemTestDataBuilder().build());
         RepositorioVenta repositorioVenta = Mockito.mock(RepositorioVenta.class);
         RepositorioItem repositorioItem = Mockito.mock(RepositorioItem.class);
         DaoVenta daoVenta = Mockito.mock(DaoVenta.class);
         DaoItem daoItem = Mockito.mock(DaoItem.class);
         Mockito.when(repositorioVenta.existePorId(Mockito.anyLong())).thenReturn(true);
         Mockito.when(daoVenta.obtenerPorId(Mockito.anyLong())).thenReturn(dtoVenta);
+        Mockito.when(daoItem.listarPorIds(Mockito.anyList())).thenReturn(dtoItemsExistentes);
         ServicioEliminarVenta servicioEliminarVenta = new ServicioEliminarVenta(repositorioVenta, repositorioItem, daoVenta, daoItem);
         // act
         servicioEliminarVenta.ejecutar(1L);
